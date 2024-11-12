9b9751e6f5926a0c99e648e7336d1429f6f8ec4c
ankitpandey71 <ankitpandeyxy@gmail.com>
2024-11-12 15:15:51 +0530

    [FIX] vector icon issue

--- App.jsx
+++ App.jsx
@@ -1,13 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
+
+import {SafeAreaView, StyleSheet, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import HomeScreen from './screens/HomeScreen';
-import {SafeAreaView} from 'react-native';
 
 function App() {
+  // const isDarkMode = useColorScheme() === 'light';
+
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
+
   return (
-    <SafeAreaView>
-      <HomeScreen />
-    </SafeAreaView>
+    // <SafeAreaView>
+    <HomeScreen />
+    // </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 export default App;
